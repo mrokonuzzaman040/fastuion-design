b02cacd397dbd54164269452c5177600f3891956
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-03-07 01:48:47 +0600

    Add Footer component to HomeLayout

--- src/Home/HomeLayout.jsx
+++ src/Home/HomeLayout.jsx
@@ -4,6 +4,7 @@ import "preline/preline.js";
 import Home from "./Home/Home";
 import Navbar from "../Components/Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer";
 
 const HomeLayout = () => {
 
@@ -11,7 +12,7 @@ const HomeLayout = () => {
         <div className="">
             <Navbar />
             <Outlet />
-            
+            <Footer />
         </div>
     );
 };
