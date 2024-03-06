814d32d51bbbb9499a2f81f9c269d4374a74e5ef
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-03-07 03:54:19 +0600

    Update package.json and About.jsx***
    ***Add react-icons dependency to package.json and update heading in About.jsx

--- src/Home/Home/About.jsx
+++ src/Home/Home/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div id='about' className='max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-24 space-y-8'>
             <div className=" p-4">
-                <h2 className="text-2xl font-bold mb-4">About Us</h2>
+                <h2 className="text-2xl font-bold mb-4">About Me</h2>
                 <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed euismod, nunc vel aliquam tincidunt, nisl nunc aliquet nunc, vitae lacinia nunc nunc id nunc. Sed euismod, nunc vel aliquam tincidunt, nisl nunc aliquet nunc, vitae lacinia nunc nunc id nunc.</p>
             </div>
         </div>
