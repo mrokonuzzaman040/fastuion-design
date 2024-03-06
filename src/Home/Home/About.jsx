af3d5e6388305e626e4054eb28c98e9d492620fa
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-03-07 02:03:12 +0600

    Update About section and assets in index.html

--- src/Home/Home/About.jsx
+++ src/Home/Home/About.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div id='about'
-            className='w-full h-screen flex justify-center items-center'
-        >
+        <div id='#about'>
             <h2>About section</h2>
         </div>
     );
