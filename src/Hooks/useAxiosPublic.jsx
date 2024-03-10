ac1919df540ad933217a45029067407eb9de1881
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-03-10 21:12:56 +0600

    Update axios baseURL in useAxiosPublic.jsx

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create( {
-    baseURL: 'http://localhost:5000/api/v1/public/',
+    baseURL: 'http://localhost:5000/',
 } );
 
 const usePublicApi = () => {
