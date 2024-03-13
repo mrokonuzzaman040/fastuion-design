e8b86ff7a03c894a827249777d6e96c91f710c07
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-03-14 00:58:51 +0600

    Refactor ProjectDetails component

--- src/Admin/Dashboard/AddItem.jsx
+++ src/Admin/Dashboard/AddItem.jsx
@@ -2,71 +2,125 @@ import React, { useState } from 'react';
 import usePublicApi from '../../Hooks/useAxiosPublic';
 
 const AddItem = () => {
-    const [ name, setName ] = useState( '' );
-    const [ description, setDescription ] = useState( '' );
-    const [ image, setImage ] = useState( null );
-    const [ cover, setCover ] = useState( null );
-
     const publicApi = usePublicApi();
-
-    const uploadImage = async () => {
-        const formData = new FormData();
-        formData.append( 'image', image );
-        try {
-            const res = await publicApi.post( '/image', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                },
-                onUploadProgress: ( progressEvent ) => {
-                    const progress = Math.round( ( progressEvent.loaded / progressEvent.total ) * 100 );
-                    console.log( progress );
-                }
-            } );
-            console.log( res.data.profile_url );
-        } catch ( error ) {
-            console.log( error );
-        }
-    };
+    const [ image, setImage ] = useState( '' );
+    const [ imgUrl, setImgUrl ] = useState( '' );
+    const [ projectImageurls, setProjectImageUrls ] = useState( [] );
+    const [ projectImages, setProjectImages ] = useState( [] );
+    const [ uploadProgress, setUploadProgress ] = useState( 0 );
 
     const uploadCover = async () => {
         const formData = new FormData();
-        formData.append( 'cover', cover );
+        formData.append( 'image', image );
         try {
-            const res = await publicApi.post( '/image', formData, {
+            const res = await publicApi.post( 'upload/coverImages', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 },
                 onUploadProgress: ( progressEvent ) => {
+                    // @ts-ignore
                     const progress = Math.round( ( progressEvent.loaded / progressEvent.total ) * 100 );
-                    console.log( progress );
+                    setUploadProgress( progress );
                 }
             } );
-            console.log( res.data.profile_url );
+            setImgUrl( res.data.image_url );
         } catch ( error ) {
             console.log( error );
         }
-    };
+    }
 
-    const handleSubmit = ( e ) => {
-        e.preventDefault();
+    // console.log( projectImages );
+    const uploadProjectImages = async () => {
         const formData = new FormData();
-        formData.append( 'name', name );
-        formData.append( 'description', description );
-        formData.append( 'image', image );
-        formData.append( 'cover', cover );
-        publicApi.post( 'api/project', formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        } ).then( ( res ) => {
-            console.log( res );
-        } ).catch( ( error ) => {
+        for ( let i = 0; i < projectImages.length; i++ ) {
+            formData.append( `image`, projectImages[ i ] );
+        }
+        try {
+            const res = await publicApi.post( 'upload/multipleImages', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                },
+                onUploadProgress: ( progressEvent ) => {
+                    // @ts-ignore
+                    const progress = Math.round( ( progressEvent.loaded / progressEvent.total ) * 100 );
+                    setUploadProgress( progress );
+                }
+            } );
+            setProjectImageUrls( res.data.image_urls );
+        } catch ( error ) {
             console.log( error );
-        } );
+        }
+    }
+
+    const handleSubmit = async ( e ) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const description = e.target.description.value;
+        const data = {
+            name,
+            description,
+            cover_image: imgUrl,
+            project_image: projectImageurls,
+        }
+
+        try {
+            const response = await publicApi.post( 'api/project', data );
+            console.log( response );
+        } catch ( error ) {
+            console.log( error );
+        }
+        console.log( data );
     };
 
     return (
         <div className="container mx-auto">
+            <div className="max-w-lg mx-auto">
+                <div className="">
+                    <div className="">
+                        <label htmlFor="image" className="block text-gray-700 text-sm font-bold mb-2">
+                            Cover Image:
+                        </label>
+                        <input
+                            type="file"
+                            id="image"
+                            // @ts-ignore
+                            onChange={ ( e ) => setImage( e.target.files[ 0 ] ) }
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                    </div>
+                    <div className="">
+                        <button
+                            onClick={ uploadCover }
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        >
+                            Upload Cover
+                        </button>
+                    </div>
+                </div>
+                <div className="mb-4">
+                    <div className="">
+                        <label htmlFor="image" className="block text-gray-700 text-sm font-bold mb-2">
+                            Project Image:
+                        </label>
+                        <input
+                            type="file"
+                            id="images"
+                            multiple
+                            // @ts-ignore
+                            onChange={ ( e ) => { setProjectImages( e.target.files ) } }
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                    </div>
+                    <div className="">
+                        <button
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            onClick={ uploadProjectImages }
+                        >
+                            Upload Images
+                        </button>
+                    </div>
+                </div>
+            </div>
             <form onSubmit={ handleSubmit } className="max-w-lg mx-auto">
                 <div className="mb-4">
                     <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">
@@ -75,7 +129,7 @@ const AddItem = () => {
                     <input
                         type="text"
                         id="name"
-                        onChange={ ( e ) => setName( e.target.value ) }
+
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     />
                 </div>
@@ -85,33 +139,11 @@ const AddItem = () => {
                     </label>
                     <textarea
                         id="description"
-                        onChange={ ( e ) => setDescription( e.target.value ) }
+
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     />
                 </div>
-                <div className="">
-                    <label htmlFor="image" className="block text-gray-700 text-sm font-bold mb-2">
-                        Cover Image:
-                    </label>
-                    <input
-                        type="file"
-                        id="image"
-                        onChange={ ( e ) => setCover( e.target.files[ 0 ] ) }
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="image" className="block text-gray-700 text-sm font-bold mb-2">
-                        Project Image:
-                    </label>
-                    <input
-                        type="file"
-                        id="image"
-                        multiple
-                        onChange={ ( e ) => setImage( e.target.files ) }
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    />
-                </div>
+
                 <div className="flex justify-center">
                     <button
                         type="submit"
@@ -121,7 +153,7 @@ const AddItem = () => {
                     </button>
                 </div>
             </form>
-        </div>
+        </div >
     );
 };
 
--- src/Home/Home/Component/ProjectDetails.jsx
+++ src/Home/Home/Component/ProjectDetails.jsx
@@ -3,14 +3,16 @@ import { useLoaderData } from 'react-router-dom';
 
 const ProjectDetails = () => {
     const data = useLoaderData();
+    // @ts-ignore
     const detailData = data[ 0 ];
+    const { name, description, cover_image, project_images } = detailData;
     console.log( data );
     return (
         <>
             <div className="max-w-4xl mx-auto p-10">
-                <h2 className="text-2xl font-bold mb-5">{ detailData.name }</h2>
-                <img src={ detailData.cover_image } alt={ detailData.name } className="w-full h-96 object-cover mb-5" />
-                <p className="mb-5">{ detailData.description }</p>
+                <h2 className="text-2xl font-bold mb-5">{ name }</h2>
+                <img src={ detailData.cover_image } alt={ cover_image } className="w-full h-96 object-cover mb-5" />
+                <p className="mb-5">{ description }</p>
                 {
                     // detailData.images && detailData.images.map( ( image, index ) => (
                     //     <img key={ index } src={ image } alt={ detailData.name } className="w-full h-96 object-cover mb-5" />
